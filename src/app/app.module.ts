3ae80ee2aa61faab4f866ec3a793f73321b6ecb2
CamilaCorigliano <64080287+CamilaCorigliano@users.noreply.github.com>
2023-10-20 22:16:21 -0300

    se crean nuevos componentes y se actualiza el app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { PageLoginComponent } from './components/page-login/page-login.component';
+import { PageLogoutComponent } from './components/page-logout/page-logout.component';
+import { RegisterAddComponent } from './components/register-add/register-add.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    PageLoginComponent,
+    PageLogoutComponent,
+    RegisterAddComponent
   ],
   imports: [
     BrowserModule,
