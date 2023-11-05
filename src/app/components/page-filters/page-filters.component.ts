e84c38685a5eee01a3a3799216f9e3f57173681a
CamilaCorigliano <64080287+CamilaCorigliano@users.noreply.github.com>
2023-11-04 22:00:54 -0300

    desarrollo de filtrado por name restaurant usando pipe

--- src/app/components/page-filters/page-filters.component.ts
+++ src/app/components/page-filters/page-filters.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { RestaurantService } from 'src/app/services/restaurant.service/restaurant.service';
 
 @Component({
   selector: 'app-page-filters',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-filters.component.css']
 })
 export class PageFiltersComponent {
+
+  
+  nameFilter = new FormControl('');
+  searchTerm: string = '';
+
+  constructor(private restaurantService: RestaurantService) {}
+
+  applyFilters() {
+    if (this.searchTerm !== '') {
+      // Filtra los restaurantes por nombre
+      this.restaurantService.applyFilters(this.searchTerm);
+    } else {
+      // Si searchTerm está vacío, restaura la lista original
+      this.restaurantService.restoreOriginalList();
+    }
+  }
+
+  onInputChange(event: Event): void {
+    const inputElement = (event.target as HTMLInputElement);
+    if (inputElement && inputElement.value) {
+      this.searchTerm = inputElement.value;
+    } else {
+      this.searchTerm = '';
+    }
+  }
+  
+
 
 }
