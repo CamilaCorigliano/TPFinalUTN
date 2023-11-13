fdbef3a2841367ed3af0f61ce26a68a5ef0bf622
juan pablo bustamante <bustamante.jpm@gmail.com>
2023-11-13 11:54:17 -0300

    routing fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+
+// Importaciones de Angular Material
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatNativeDateModule } from '@angular/material/core';
+
+
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { PageLoginComponent } from './components/page-login/page-login.component';
 import { PageLogoutComponent } from './components/page-logout/page-logout.component';
 import { RegisterAddComponent } from './components/register-add/register-add.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RestaurantListComponent } from './components/restaurant/restaurant-list/restaurant-list.component';
 import { PageFiltersComponent } from './components/page-filters/page-filters.component';
-import { HttpClientModule } from '@angular/common/http';
 import { RestaurantService } from './services/restaurant.service/restaurant.service';
 import { NameFilterPipe } from './name-filter.pipe';
 import { RestaurantViewComponent } from './components/restaurant/restaurant-view/restaurant-view.component';
 import { PageReservationComponent } from './components/page-reservation/page-reservation.component';
 
-
 @NgModule({
-  
   declarations: [
     AppComponent,
     PageNotFoundComponent,
@@ -38,7 +46,9 @@ import { PageReservationComponent } from './components/page-reservation/page-res
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [RestaurantService],
   bootstrap: [AppComponent]
