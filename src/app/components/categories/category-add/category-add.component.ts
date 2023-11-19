c09215ff3b40acfa2816eebc7e68207db764b41f
CamilaCorigliano <64080287+CamilaCorigliano@users.noreply.github.com>
2023-11-18 22:27:04 -0300

    avances en la funcionalidad para asociar nuevas categorias al restaurant

--- src/app/components/categories/category-add/category-add.component.ts
+++ src/app/components/categories/category-add/category-add.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { userService } from 'src/app/services/api.service/userService';
 import { CategoryService } from 'src/app/services/category.service/category.service';
 import { RestaurantService } from 'src/app/services/restaurant.service/restaurant.service';
+import { userService } from 'src/app/services/api.service/userService';
 
 @Component({
   selector: 'app-category-add',
@@ -12,41 +11,59 @@ import { RestaurantService } from 'src/app/services/restaurant.service/restauran
 })
 export class CategoryAddComponent {
 
-  userRestaurant! : any;
+  userRestaurant: any;
   categoryForm: FormGroup;
+  allCategories: string[] = ['parrilla', 'sushi', 'vegano', 'pasta', 'italiana', 'china', 'rapida', 'pescado', 'cafeteria', 'pizza', 'hamburguesas', 'bar', 'vinoteca'];
+  availableCategories: string[] = [];
 
-  constructor(private restaurantService: RestaurantService, private categoryService: CategoryService, private userService: userService) {
-
+  constructor(
+    private restaurantService: RestaurantService,
+    private categoryService: CategoryService,
+    private userService: userService
+  ) {
     this.categoryForm = new FormGroup({
       category: new FormControl('', Validators.required)
     });
-
   }
 
   ngOnInit(): void {
+    this.getRestaurantForUser();
+    this.updateAvailableCategories();
+  }
+
+  getRestaurantForUser() {
     if (this.userService.user && this.restaurantService.restaurants) {
-       this.userRestaurant = this.restaurantService.restaurants.find(
+      this.userRestaurant = this.restaurantService.restaurants.find(
         (restaurant) => restaurant.manager_id === this.userService.user._id
       );
 
-       }
-      
-
-
+      console.log("Categorías del restaurante:", this.userRestaurant.categories);
+    }
   }
 
-  onSubmit(){
+  updateAvailableCategories() {
+    if (this.userRestaurant) {
+      // Filtrar las categorías disponibles
+      this.availableCategories = this.allCategories.filter(category => !this.userRestaurant.categories.includes(category));
+    }
+  }
 
-    const { category} = this.categoryForm.value;
+  onSubmit() {
+    const { category } = this.categoryForm.value;
 
-    this.categoryService.createCategory(this.userRestaurant.id, category)
+    if (this.userRestaurant) {
+      this.categoryService.createCategory(this.userRestaurant.id, category)
         .subscribe(
-          response =>  {this.categoryForm.reset()},
-          error => console.error('Error de la API:', error)
+          response => {
+            console.log('Categoría creada exitosamente:', response);
+            this.categoryForm.reset();
+            this.updateAvailableCategories(); // Actualizar las categorías disponibles después de crear una nueva
+          },
+          error => {
+            console.error('Error al crear la categoría:', error);
+          }
         );
-
+    }
   }
 
 }
-
-
