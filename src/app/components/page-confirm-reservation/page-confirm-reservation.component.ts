f946ec1143c450fd83a6ba69010e00ac93e9b6c1
juan pablo bustamante <bustamante.jpm@gmail.com>
2023-11-21 07:33:35 -0300

    confirm reservation

--- src/app/components/page-confirm-reservation/page-confirm-reservation.component.ts
+++ src/app/components/page-confirm-reservation/page-confirm-reservation.component.ts
@@ -4,24 +4,36 @@ import { ReservationService } from 'src/app/services/reservation.service/reserva
 import { RestaurantService } from 'src/app/services/restaurant.service/restaurant.service';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TableService } from 'src/app/services/table.service/table.service';
 
 @Component({
   selector: 'app-page-confirm-reservation',
   templateUrl: './page-confirm-reservation.component.html',
-  styleUrls: ['./page-confirm-reservation.component.css']
+  styleUrls: ['./page-confirm-reservation.component.css'],
 })
 export class PageConfirmReservationComponent {
   userRestaurant!: any;
   tablesReserved!: any[];
   reservation!: any;
- 
+  tables!: any[];
+  availableTables!: any[];
+  reservationId!:any;
 
-  confirmationForm  = new FormGroup({
-    table: new FormControl('', Validators.required)
+  confirmationForm = new FormGroup({
+    table: new FormControl([] as any[], Validators.required),
   });
+  
 
-  constructor(private userService: userService, private restaurantService: RestaurantService, private reservationService: ReservationService,
-    private route: ActivatedRoute) {}
+  constructor(
+    private userService: userService,
+    private restaurantService: RestaurantService,
+    private reservationService: ReservationService,
+    private route: ActivatedRoute,
+    private tableServise: TableService
+  ) {}
+  get tableControl() {
+    return this.confirmationForm.get('table') as FormControl;
+  }
 
   ngOnInit() {
     if (this.userService.user && this.restaurantService.restaurants) {
@@ -29,27 +41,79 @@ export class PageConfirmReservationComponent {
         (restaurant) => restaurant.manager_id === this.userService.user._id
       );
     }
-    
-    this.route.params.subscribe(params => {
-      const reservationId = params['id'];
-      this.reservationService.getById(reservationId).subscribe(
-        data => {
+
+    this.route.params.subscribe((params) => {
+       this.reservationId = params['id'];
+      this.reservationService.getById(this.reservationId).subscribe(
+        (data) => {
           this.reservation = data;
+          this.reservationService
+            .getTablesByDate(this.userRestaurant.id, this.reservation.due_date)
+            .subscribe(
+              (data) => {
+                this.tablesReserved = data;
+                this.tableServise.getTables(this.userRestaurant.id).subscribe(
+                  (data) => {
+                    this.tables = data;
+                    if (this.tablesReserved.length > 0) {
+                      this.tables.forEach((table) => {
+                        this.tablesReserved.forEach((tableReserved) => {
+                          if (table.number != tableReserved.table_number) {
+                            this.tables.push(table.number);
+                          }
+                        });
+                      });
+                    } else {
+                      this.availableTables = this.tables;
+                    }
+                  },
+                  (error) => {
+                    console.log(error);
+                  }
+                );
+              },
+              (error) => {
+                console.log(error);
+              }
+            );
         },
-        error => {
+        (error) => {
           console.log(error);
         }
       );
     });
-    
+  }
 
-   
-
+  onSubmit() {
+    if (this.confirmationForm?.valid) {
+      const tableControl = this.confirmationForm.get('table');
   
- 
-    
+      console.log('tableControl:', tableControl?.value);
+  
+      if (tableControl instanceof FormControl && Array.isArray(tableControl.value)) {
+        let selectedTables: number[] = tableControl.value;
+
+        console.log('selectedTables:', selectedTables);
+        console.log(this.reservationId);
+        if (selectedTables.length > 0) {
+          this.reservationService
+            .confirmReservation(this.userRestaurant.id, this.reservationId, 'confirmed', selectedTables)
+            .subscribe(
+              (data) => {
+                alert('Reserva confirmada con éxito');
+              },
+              (error) => {
+                console.error('Error al confirmar la reserva:', error.message);
+                alert('Error al confirmar la reserva. Consulta la consola para más detalles.');
+              }
+            );
+        } else {
+          alert('Selecciona al menos una mesa antes de confirmar la reserva.');
+        }
+      }
+    }
   }
-  onSubmit(){
-   
-  }
+  
+  
 }
+
