d51ae6a3f0997a86f37191f7bd85e7624587055c
juan pablo bustamante <bustamante.jpm@gmail.com>
2023-11-21 08:13:02 -0300

    mejor reservation view

--- src/app/components/page-reservation-view/page-reservation-view.component.ts
+++ src/app/components/page-reservation-view/page-reservation-view.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ReservationService } from 'src/app/services/reservation.service/reservation.service';
 import { Router } from '@angular/router';
+import { userService } from 'src/app/services/api.service/userService';
 @Component({
   selector: 'app-page-reservation-view',
   templateUrl: './page-reservation-view.component.html',
@@ -9,16 +10,18 @@ import { Router } from '@angular/router';
 })
 export class PageReservationViewComponent {
   reservation!: any;
+  user!: any;
 
-  constructor(private route: ActivatedRoute, private reservationService: ReservationService, private router: Router) {}
+  constructor(private route: ActivatedRoute, private reservationService: ReservationService, private router: Router, private userService: userService) {}
 
   ngOnInit(): void {
+    this.user = this.userService.user;
+    console.log(this.user);
     this.route.params.subscribe(params => {
       const reservationId = params['id'];
       this.reservationService.getById(reservationId).subscribe(
         data => {
           this.reservation = data;
-          console.log(this.reservation);
           
         },
         error => {
