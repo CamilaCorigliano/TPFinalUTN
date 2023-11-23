4dc18bc25d714a5e5e19773df62ca5a98b63b54a
CamilaCorigliano <64080287+CamilaCorigliano@users.noreply.github.com>
2023-11-22 22:46:25 -0300

    carga dinamica de las imagenes y el link de map dependiendo el restaurant name

--- src/app/components/restaurant/restaurant-view/restaurant-view.component.ts
+++ src/app/components/restaurant/restaurant-view/restaurant-view.component.ts
@@ -31,6 +31,52 @@ export class RestaurantViewComponent {
 
   }
 
+  getRestaurantImage(restaurantName: string): string {
+    switch (restaurantName) {
+      case 'Manolo Centro ':
+        return `assets/images/manolo.jpeg`;
+      case 'nombre2':
+        return `assets/images/manolo.jpeg`;
+      default:
+        return `assets/images/manolo.jpeg`;
+    }
+    
+  }
+  
+  getRestaurantSchedule(restaurantName: string): string[] {
+    switch (restaurantName) {
+      case 'Manolo Centro ':
+        return ['Lunes de 08:00 am a 02:00 pm', 'Martes de 08:00 am a 02:00 pm', 'Miercoles de 08:00 am a 02:00 pm', 'Jueves de 08:00 am a 02:00 pm',
+                'Viernes de 08:00 am a 02:00 pm', 'Sábado de 08:00 am a 02:00 pm', 'Domingo de 08:00 am a 02:00 pm'];
+      case 'nombre2':
+        return ['Lunes de 09:00 am a 03:00 pm', 'Miércoles de 09:00 am a 03:00 pm', /* ... otros días */];
+      default:
+        return [];
+    }
+  }
+  
+  getRestaurantLink(restaurantName: string): string {
+    switch (restaurantName) {
+      case 'Manolo Centro ':
+        return 'https://www.google.com/maps/search/manolo+centro+mar+del+plata/@-38.0003278,-57.5567473,14z?entry=ttu';
+      case 'nombre2':
+        return 'https://www.ejemplo2.com';
+      default:
+        return '#'; // Enlace predeterminado si no se encuentra el nombre del restaurante
+    }
+  }
+  
+  getMapImage(restaurantName: string): string {
+    switch (restaurantName) {
+      case 'Manolo Centro ':
+        return `assets/images/mapamanolocentro.jpeg`;
+      case 'nombre2':
+        return `assets/images/manolo.jpeg`;
+      default:
+        return `assets/images/manolo.jpeg`;
+    }
+  }
+
   
 
 
